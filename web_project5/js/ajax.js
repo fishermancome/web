b36d2448caca076fdbed90c73c59e1835383e583
fisherman <1553853517@qq.com>
2016-08-13 20:35:25 +0800

    web_pro5

---
+++ web_project5/js/ajax.js
@@ -0,0 +1,11 @@
+	featuredcontentslider.init({
+		id: "slider4", 
+		contentsource: ["inline", ""], 
+		toc: "#increment", 
+		nextprev: ["", ""],
+		revealtype: "mouseover", 
+		enablefade: [true, 0.1], 
+		autorotate: [true, 3500], 
+		onChange: function(previndex, curindex){ 
+	}
+})
